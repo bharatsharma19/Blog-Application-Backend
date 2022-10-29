f7a0f914b551dbb42ac88e13b3b36f5aa005481f
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-29 12:19:28 +0530

    Updated

--- Routes/auth.js
+++ Routes/auth.js
@@ -23,14 +23,22 @@ router.post("/register", async (req, res) => {
 // Login
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong credentials!");
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      !user && res.status(400).json("Wrong credentials!");
 
-    const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong credentials!");
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      !validated && res.status(400).json("Wrong credentials!");
 
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+      if (!user && !validated) {
+        res.status(400).json("Wrong credentials!");
+      }
+
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   } catch (err) {
     res.status(500).json(err);
   }
